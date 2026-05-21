a8563a22839ebcecd8e2f261aaf9182e6a43298b
nirb <nir.bejerano@gmail.com>
2026-05-21 09:58:58 +0300

    Bot update: new sw.js

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'portfolio-v1';
+const ASSETS = [
+  '/watcher_bot_site/',
+  '/watcher_bot_site/index.html',
+  '/watcher_bot_site/manifest.json',
+  '/watcher_bot_site/icon-192.png',
+  '/watcher_bot_site/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((response) => {
+        if (response && response.status === 200 && response.type === 'basic') {
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(e.request, responseToCache);
+          });
+        }
+        return response;
+      })
+      .catch(() => {
+        return caches.match(e.request);
+      })
+  );
+});
